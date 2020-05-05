cc7f7bd869426658143597820ff21cc151c99969
maciek <ignaszov@o2.pl>
2020-05-05 20:50:16 +0200

    commit 1 test file

---
+++ fun1.py
@@ -0,0 +1,11 @@
+
+
+def fun1(a, b, c):
+    out = a + b + 100
+    if c != 0:
+        out = out / c
+    else:
+        out = out / 1.5
+    print(out)
+
+
---
+++ toRun.py
@@ -0,0 +1,20 @@
+import os
+import sys
+os.listdir()
+
+x = 5
+y = 6
+z = 7
+
+
+def test(a, b, c):
+    out = a+b+c
+    print(out)
+
+
+test(x, 2*x, x)
+fun1(x, x, x)
+fun2(x, x, 0)
+
+
+
